a85de3b2c79cf81cf20949fc0c0b1c978adbc76b
Vicent Pérez <vicennt@gmail.com>
2022-06-16 15:26:16 +0200

    generate projects module with basic CRUD

---
+++ src/projects/controllers/projects.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { ProjectsService } from '../services/projects.service';
+
+@Controller('projects')
+export class ProjectsController {
+  constructor(private readonly projectsService: ProjectsService) {}
+
+  @Post()
+  create(@Body() createProjectDto) {
+    return this.projectsService.create(createProjectDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.projectsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.projectsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateProjectDto) {
+    return this.projectsService.update(+id, updateProjectDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.projectsService.remove(+id);
+  }
+}
